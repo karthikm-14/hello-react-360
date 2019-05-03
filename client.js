aef3bb75f5b296bb34a632049303d8b6d05341a8
Karthik Mahadevaiah <karthik.iah@gmail.com>
2019-05-03 11:54:28 +0530

    flat capsule added

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import {ReactInstance, Surface} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
+  const SIZE = 300;
   const r360 = new ReactInstance(bundle, parent, {
     // Add custom options here
     fullScreen: true,
@@ -11,10 +12,24 @@ function init(bundle, parent, options = {}) {
   });
 
   // Render your app content to the default cylinder surface
+  // r360.renderToSurface(
+  //   r360.createRoot('hello_react_360', { /* initial props */ }),
+  //   r360.getDefaultSurface()
+  // );
+
+  // CAPSULE 
+  const capsuleSurface = new Surface(
+    SIZE,
+    SIZE,
+    Surface.SurfaceShape.Flat
+  )
+  capsuleSurface.setAngle(Math.PI/2, 0);
   r360.renderToSurface(
-    r360.createRoot('hello_react_360', { /* initial props */ }),
-    r360.getDefaultSurface()
-  );
+    r360.createRoot('Info', {name: 'Csapsule'}),
+    capsuleSurface
+  )
+
+  const sp
 
   // Load the initial environment
   r360.compositor.setBackground(r360.getAssetURL('360_world.jpeg'));
